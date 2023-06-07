838362bc0d2349c545b499639aedf5a75b9c10d0
Valdess <132627914+FernandoValdes01@users.noreply.github.com>
2023-06-06 20:43:12 -0400

    proyecto (2).py

--- proyecto (2).py
+++ proyecto (2).py
@@ -1,68 +1,75 @@
 import numpy as np
 import matplotlib.pyplot as plt
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
-import tkinter as tk
+import customtkinter as ctk
+
+def calcular_Trayectoria(velocidad, angulo):
+    # Convierto el angulo a radians
+    angle_rad = np.deg2rad(angulo)
+
+    # Calculo el tiempo de vuelo
+    tiempo_de_vuelo = (2 * velocidad * np.sin(angle_rad)) / 9.8
+
+    # Calculo el timpo en intervalos
+    t = np.linspace(0, tiempo_de_vuelo, num=100)
+
+    # Calculo el "x" y "y" posiciones en cada  intervalo del tiempo
+    x = velocidad * np.cos(angle_rad) * t
+    y = velocidad * np.sin(angle_rad) * t - 0.5 * 9.8 * t ** 2
+
+    # Encuentra la altura maxima y el tiempo correspondiente
+    Altura_maxima = np.max(y)
+    Altura_maximo_tiempo = t[np.argmax(y)]
+
+    return x, y, Altura_maxima, Altura_maximo_tiempo
 
 
-def calculate_trajectory(velocity, angle):
-    # Convert the angle to radians
-    angle_rad = np.deg2rad(angle)
-
-    # Calculate the time of flight
-    time_of_flight = (2 * velocity * np.sin(angle_rad)) / 9.8
-
-    # Calculate the time intervals
-    t = np.linspace(0, time_of_flight, num=100)
-
-    # Calculate the x and y positions at each time interval
-    x = velocity * np.cos(angle_rad) * t
-    y = velocity * np.sin(angle_rad) * t - 0.5 * 9.8 * t ** 2
-
-    # Find the maximum height and corresponding time
-    max_height = np.max(y)
-    max_height_time = t[np.argmax(y)]
-
-    return x, y, max_height, max_height_time
-
-
-def plot_trajectory(x, y, max_height, max_height_time):
-    fig, ax = plt.subplots()
+def mostrar_Trayectoria(x, y, Altura_Maxima, Altura_maximo_tiempo):
+    fig, ax = plt.subplots(dpi = 150)
     ax.plot(x, y)
-    ax.plot(max_height_time, max_height, 'ro')
-    ax.set_xlabel('Distance (m)')
-    ax.set_ylabel('Height (m)')
-    ax.set_title('Projectile Motion')
+    ax.plot(Altura_maximo_tiempo, Altura_Maxima, 'ro')
+    ax.set_xlabel('Distancia (Metros)')
+    ax.set_ylabel('Altura (Metros)')
+    ax.set_title('Demostración del proyectil')
     plt.show()
+    
+    def on_submit():
+    velocidad = float(velocity_entry.get())
+    angulo= float(angle_entry.get())
 
-
+    x, y, Altura_maxima, Altura_maxima_tiempo= calcular_Trayectoria(velocidad, angulo)
+    mostrar_Trayectoria(x, y, Altura_maxima, Altura_maxima_tiempo)
+    
+    
+    
 def on_submit():
-    velocity = float(velocity_entry.get())
-    angle = float(angle_entry.get())
+    velocidad = float(velocity_entry.get())
+    angulo= float(angle_entry.get())
 
-    x, y, max_height, max_height_time = calculate_trajectory(velocity, angle)
-    plot_trajectory(x, y, max_height, max_height_time)
+    x, y, Altura_maxima, Altura_maxima_tiempo= calcular_Trayectoria(velocidad, angulo)
+    mostrar_Trayectoria(x, y, Altura_maxima, Altura_maxima_tiempo)
 
+# Crea las ventanas GUI
+window = ctk.CTk()
+window.title("Proyección de lanzamiento")
+window.configure(bg_color ='transparent')
+window.geometry("300x250")
 
-# Create the GUI window
-window = tk.Tk()
-window.title("Projectile Motion")
-window.configure(bg='black')
-
-# Create the velocity input label and entry
-velocity_label = tk.Label(window, text="Initial Velocity (m/s):", fg='white', bg='black')
-velocity_label.pack()
-velocity_entry = tk.Entry(window)
+# Crear la entrada y la etiqueta de entrada de velocidad
+velocity_label =ctk.CTkLabel(window, text="Velocidad Inicial(m/s):", fg_color='transparent', bg_color ='transparent')
+velocity_label.pack(padx = 10 , pady = 10)
+velocity_entry = ctk.CTkEntry(window)
 velocity_entry.pack()
 
-# Create the angle input label and entry
-angle_label = tk.Label(window, text="Launch Angle (degrees):", fg='white', bg='black')
-angle_label.pack()
-angle_entry = tk.Entry(window)
+# Crear la entrada y la entrada y la etiqueta de entrada de ángulo
+angle_label = ctk.CTkLabel(window, text="Angulo de Lanzamiento:", fg_color='transparent', bg_color ='transparent')
+angle_label.pack(padx = 10 , pady = 10)
+angle_entry = ctk.CTkEntry(window)
 angle_entry.pack()
 
-# Create the submit button
-submit_button = tk.Button(window, text="Submit", command=on_submit)
-submit_button.pack()
+# Crear el boton de enviar
+submit_button = ctk.CTkButton(window, text="Aceptar", command=on_submit)
+submit_button.pack(padx = 20 , pady = 20)
 
-# Run the GUI event loop
+# Inicia el GUI en un bucle de eventos
 window.mainloop()
