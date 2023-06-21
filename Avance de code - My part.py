5d557b7e780d40aca0235d667eab92cbc8e7334a
LuisCerda07 <134728490+LuisCerda07@users.noreply.github.com>
2023-06-21 01:08:16 -0400

    Update Avance de code - My part.py
    Pequeña Actualizacion

--- Avance de code - My part.py
+++ Avance de code - My part.py
@@ -1,57 +1,27 @@
+#----------------IMPORTAMOS LIBRERIAS---------------------------#
+#import tkinter
 import customtkinter as ctk
 import matplotlib.pyplot as plt
 from matplotlib.figure import Figure
-from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
+from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg, NavigationToolbar2Tk
 import numpy as np
+import webbrowser
 
+#-------------------------------VENTANA PRINCIPAL---------------#
 main_window = ctk.CTk()                                                                                #   Asignamos las propiedades de ctk.Ctk a main_window
-main_window.title("Tiro Parabolico")                                                                            #   Ponemos nombre a la ventanita creada
+main_window.title("Tiro Parabolico")                                                                   #   Ponemos nombre a la ventanita creada
 main_window.geometry("900x600")                                                                         #   Definimos la resolucion de la ventana
 main_window_width, main_window_height = main_window.winfo_geometry()[0], main_window.winfo_geometry()[1] #   Obtenemos el alto y ancho
 main_window.resizable(False, False)
 
-graphic_frame = ctk.CTkFrame(main_window, width=643, height=480)    #Frame para el gráfico  "graphic_frame"
-graphic_frame.grid(row=0, column=1, pady=20) 
+
+#-----------------------------FUNCIONES Y CALCULOS----------------#
+
+num_parts = 35
+toolbar = None
 
 
-# Parámetros del tiro parabólico
-#g = 9.8  # Aceleración gravitacional
-#v0 = velocity_entry  # Velocidad inicial
-#angle = 45  # Ángulo de lanzamiento
-
-buttons_frame = ctk.CTkFrame(main_window, width=800, height=450)    #Frame para Botones/textboxes   "buttons_frame"
-buttons_frame.grid(row=0, column=0, padx=20, pady=10)
-
-txt1label = ctk.CTkLabel(buttons_frame, width=10, height=20, text="Velocidad Inicial:", font=ctk.CTkFont(size=12, weight="normal"))    #Crear texto dentro de buttons_frame
-txt1label.grid(row=0, column=0, columnspan=2, pady=15, sticky="sew")                                                               #Posicionamiento del texto dentro de buttons_frame
-
-angle_entry = ctk.CTkEntry(buttons_frame, width=60, height=12)                                         #Textbox para almacenar valores
-angle_entry.grid(row=1, column=0, padx=2, pady=5, sticky="ne")
-
-txt_2_label = ctk.CTkLabel(buttons_frame, width=5, height=10, text="Angulo de Lanzamiento:", font=ctk.CTkFont(size=12, weight="normal"))
-txt_2_label.grid(row=3, column=0, columnspan=2, pady=15, sticky="sew")
-velocity_entry = ctk.CTkEntry(buttons_frame, width=60, height=8)
-velocity_entry.grid(row=4, column=0, padx=2, pady=5, sticky="ne")
-
-# Cálculo de las trayectorias en 5 partes
-# Parámetros del tiro parabólico
-g = 9.8  # Aceleración gravitacional
-v0 = 2 # Velocidad inicial
-angle = 90 # Ángulo de lanzamiento
-
-
-num_parts = 10
-dt = 2 * v0* (1 / g) / num_parts
-t = 0
-x = []
-y = []
-for _ in range(num_parts + 1):
-    x.append(v0 * t * (1))
-    y.append(v0 * t * (1) - 0.5 * g * t**2)
-    t += dt
-
-
-# Crear figura de Matplotlib
+#--------CALCULAMOS NUESTRA TRAYECTORIA PRINCIPA--------------------------------------#
 def calcular_Trayectoria(velocidad, angulo):
     # Convierto el angulo a radians
     #los pasamos aradians por que asi es mas facil
@@ -62,10 +32,10 @@ def calcular_Trayectoria(velocidad, angulo):
     tiempo_de_vuelo = (2 * velocidad * np.sin(angle_rad)) / 9.8 #Ecuación Tiempo de vuelo
 
     # Calculo el timpo en intervalos
-    t = np.linspace(0, tiempo_de_vuelo, num=10)
+    t = np.linspace(0, tiempo_de_vuelo, num=35)
 
     # Calculo el "x" y "y" posiciones en cada  intervalo del tiempo
-    x = velocidad * np.cos(angle_rad) * t #Ecuación para la Posición
+    x = velocidad * np.cos(angle_rad) * t #Ecuación para la Posición en eje X
     y = velocidad * np.sin(angle_rad) * t - 0.5 * 9.8 * t ** 2 #Ecuación de Itinerario
 
     # Encuentra la altura maxima y el tiempo correspondiente
@@ -75,76 +45,78 @@ def calcular_Trayectoria(velocidad, angulo):
     return x, y, Altura_maxima, Altura_maximo_tiempo
 
 
-
-
-
-# Funciones para actualizar el gráfico
+#--------------------MOSTRAMOS GRAFICO CON MATPLOTLIB-------------------------------#
 def mostrar_plot(x, y, Altura_Maxima, Altura_maximo_tiempo,part):
     fig, ax= plt.subplots()
     #ax.plot(x, y)
     ax.plot(Altura_maximo_tiempo, Altura_Maxima)
     ax.axhline(y=Altura_Maxima, xmin=0.0, xmax=1.0, color='r')
-    ax.set_xlabel('Distancia (Metros)')
-    ax.set_ylabel('Altura (Metros)')
+    ax.set_xlabel('Distancia (Metros(MRUA))')
+    ax.set_ylabel('Altura (Metros(MRU))')
     ax.set_title(f"Simulación tiro parabólico")
+    ax.text(Altura_maximo_tiempo, Altura_Maxima, f'Altura: {Altura_Maxima:.2f} metros',
+    verticalalignment='bottom', horizontalalignment='left', color='r', fontsize=9)
 
     line, = ax.plot(x, y, 'b')
     line.set_data(x[:part+1], y[:part+1])
 
     return fig 
 
-
-#fig = Figure(figsize=(5, 4), dpi=100)
-#ax = fig.add_subplot(1, 1, 1)
-#ax.set_xlabel('Distancia (m)')
-#ax.set_ylabel('Altura (m)')
-#ax.set_xlim(0, max(x) * 1.1)
-#ax.set_ylim(0, max(y) * 1.1)
-
-
+#-------DIBUJAR PARTES DEL GRAFICO------------------#
 current_part = 0
-current_part_2 = 1
 
+#----CALCULA LA SIGUIENTE PARTE DEL GRAFICO------#
 def next_part():
     
-    global current_part, angle_entry, velocity_entry
+    global current_part, angle_entry, velocity_entry, toolbar
     angle = float(angle_entry.get())
     velocity = float(velocity_entry.get())
     
     temp1 = calcular_Trayectoria(angle, velocity)
 
     if current_part < num_parts:
-        current_part += 1
-    
+        current_part += 3
+       
     fig = mostrar_plot(temp1[0], temp1[1], temp1[2], temp1[3], current_part)
     
     canvas = FigureCanvasTkAgg(fig, master=graphic_frame)
     canvas.get_tk_widget().place(x = 2, y = 2)
-    canvas.draw()   
-        
-    return
+    canvas.draw() 
 
+    toolbar = NavigationToolbar2Tk(canvas, graphic_frame)
+    toolbar.update()
+    toolbar.pack_forget()  
+        
+    return toolbar
+
+
+#------CALCULA LA PARTE PREVIA DEL GRAFICO---------------#
 def prev_part():
-    global current_part, angle_entry, velocity_entry
+    global current_part, angle_entry, velocity_entry, toolbar
     angle = float(angle_entry.get())
     velocity = float(velocity_entry.get())
     
     temp1 = calcular_Trayectoria(angle, velocity)
 
     if current_part > 0:
-        current_part -= 1
+        current_part -= 3
 
     fig = mostrar_plot(temp1[0], temp1[1], temp1[2], temp1[3], current_part)
     
     canvas = FigureCanvasTkAgg(fig, master=graphic_frame)
     canvas.get_tk_widget().place(x = 2, y = 2)
-    canvas.draw()    
+    canvas.draw()
+
+    toolbar = NavigationToolbar2Tk(canvas, graphic_frame)
+    toolbar.update()
+    toolbar.pack_forget()      
     return 
 
-# Agregar el gráfico a la ventana Tkinter
 
+
+#-----SE REALIZA CALCULO FINAL PARA EL GRAFICO-----------#
 def calculofinal():
-    global angle_entry, velocity_entry, current_part
+    global angle_entry, velocity_entry, current_part, toolbar
 
     angle = float(angle_entry.get())
     velocity = float(velocity_entry.get())
@@ -156,21 +128,62 @@ def calculofinal():
     canvas = FigureCanvasTkAgg(fig, master=graphic_frame)
     canvas.get_tk_widget().place(x = 2, y = 2)
     canvas.draw()
-    return
+
+    return 
+
+#---------FUNCION DEL VIDEO DE YOUTUBE--------------------------------------------------#
+def reproducir_video():
+    url = "https://www.youtube.com/watch?v=2DJDTtIazgA"  
+    webbrowser.open(url)
+
+def Guardar():
+    toolbar.save_figure()
+
+#---------------------------------GRAFICOS;BOTONES;ENTRADAS------------------------------------------------#
+#------------------------------------LUGAR DEL GRAFICO-----------------------------------------------------#
+graphic_frame = ctk.CTkFrame(main_window, width=643, height=480)    #Frame para el gráfico  "graphic_frame"
+graphic_frame.grid(row=0, column=1, pady=20)
 
 
+#---------------------------------LUGAR DE ENTRADA DE DATOS------------------------------------------------#
+buttons_frame = ctk.CTkFrame(main_window, width=800, height=450)    #Frame para Botones/textboxes   "buttons_frame"
+buttons_frame.grid(row=0, column=0, padx=20, pady=10)
 
+Botonesfuncionales= ctk.CTkFrame(main_window, width=740, height=200)
+Botonesfuncionales.grid(row=1, column=0, padx=20, pady=10)
+
+
+#---------------------------------ENTRADA DE DATO=VELOCIDAD------------------------------------------------#
+txt1label = ctk.CTkLabel(buttons_frame, width=50, height=20, text="Velocidad Inicial:", font=ctk.CTkFont(size=12, weight="normal"))    #Crear texto dentro de buttons_frame
+txt1label.grid(row=0, column=0, columnspan=2, pady=15, sticky="we")                                                               #Posicionamiento del texto dentro de buttons_frame
+velocity_entry = ctk.CTkEntry(buttons_frame, width=50, height=20)
+velocity_entry.grid(row=3, column=0,columnspan=2, padx=5, pady=5, sticky="ew")
+
+
+#---------------------------------ENTRADA DE DATO=ANGULO---------------------------------------------------#
+angle_entry = ctk.CTkEntry(buttons_frame, width=50, height=20)                                         #Textbox para almacenar valores
+angle_entry.grid(row=1, column=0,columnspan=2, padx = 5, pady=5, sticky="we")
+txt_2_label = ctk.CTkLabel(buttons_frame, width=50, height=10, text="Angulo:", font=ctk.CTkFont(size=12, weight="normal"))
+txt_2_label.grid(row=2, column=0, columnspan=2, pady=15, sticky="we")
+
+
+#---------------------------------CREACION DEL BOTON=GRAFICAR----------------------------------------------#
 b_3 = ctk.CTkButton(buttons_frame, width=60, height=8, command=calculofinal, text = 'Graficar')
-b_3.grid(row=5, column=1, padx=2, pady=8, sticky="nw")
+b_3.grid(row=5, column=0,columnspan=2,padx=0, pady=0, sticky="we")
 
-# Agregar los botones
+
+#---------------------------------CREACION DE LOS BOTONES A/S----------------------------------------------#
 btn_prev = ctk.CTkButton(main_window, text="Anterior", command=prev_part)
 btn_prev.grid(row = 1, column = 1, padx = 50, columnspan=2, sticky='w')
 btn_next = ctk.CTkButton(main_window, text="Siguiente", command=next_part)
 btn_next.grid(row = 1, column = 1, padx = 40, columnspan=2, sticky='e' )
 
-# Mostrar el primer tiro parabólico
-#update_plot(0)
+buttonsave = ctk.CTkButton(Botonesfuncionales, text="Guardar", command=Guardar)
+buttonsave.grid(column=3,row=0, padx=5)
+
+#---------------------------------CREACION DEL BOTON "EJEMPLO"----------------------------------------------#
+yt = ctk.CTkButton(main_window, text="Ejemplo", command=reproducir_video)
+yt.grid(row = 1, column = 1, padx = 25, columnspan=2, sticky='s')
 
 # Ejecutar la interfaz
 main_window.mainloop()
